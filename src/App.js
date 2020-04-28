7e9e70eb0b54523533696011bac16da75efea597
Gururaj S K <gskglb@gmail.com>
2020-04-28 19:41:54 +0530

    firestore statemanagement

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/home/homepage.component';
 import ShopPage from './pages/shop/shoppage.component'; 
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.util';
+import { auth ,createUserProfileDocument } from './firebase/firebase.util';
 
 import './App.css';
 
@@ -21,10 +21,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
-      this.setState({ currentUser : user })
-      console.log(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      this.setState({ currentUser : userAuth })
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser : {
+              id : snapshot.id,
+              ...snapshot.data()
+            }
+          }, () => console.log(this.state.currentUser))
+          
+        })
+      }
     })
+
   }
 
   // This is necessary to close the open subsciption messaging channel between app and firebase to avoid any memory leaks
--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -3,6 +3,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+
 const config = {
     apiKey: "AIzaSyA3kBwxv1jQGi1XobyV7jE0n1vB2l-61uk",
     authDomain: "crwndb-7db23.firebaseapp.com",
@@ -14,6 +15,32 @@ const config = {
 }
 
 firebase.initializeApp(config)
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get()
+
+    if(!snapShot.exists) {
+        // user authenritcated but we have no user details in our db
+        const { displayName, email } = userAuth
+        const createdAt = new Date()
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }catch(error) {
+            console.log('Error creating user : ' + error.message)
+        }
+
+    }
+    return userRef
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
